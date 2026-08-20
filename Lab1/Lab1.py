5419d962f55139b03f05085391b10fc89921bec9
Faaz Shahab <fs09911@st.habib.edu.pk>
2026-08-20 17:21:33 +0500

    upload

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -24,13 +24,17 @@ class UI(QtWidgets.QMainWindow):
         self.pushButton.clicked.connect(self.handle_add_click)
         self.buttonBox.accepted.connect(self.handle_validation)
         self.buttonBox.rejected.connect(self.close)
+        self.checkBox.toggled.connect(self.disable)
+
     def handle_add_click(self):
         list1 = self.lineEdit_3.text()
         self.listWidget.addItems([list1])
         self.lineEdit_3.clear()
     def handle_validation(self):
         pass
-
+    def disable(self, checked):
+        self.lineEdit_4.setEnabled(not checked)
+        self.dateEdit_2.setEnabled(not checked)
 
 app = QtWidgets.QApplication(sys.argv)  # Create an instance of QtWidgets.QApplication
 window = UI()  # Create an instance of our class
